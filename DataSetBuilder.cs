ddec44e482311c28f8c6b7c44d4f8d4f8ec511a9
dk76 <kornilov.dima@gmail.com>
2023-10-17 18:24:31 +0300

    Init

---
+++ DataSetBuilder.cs
@@ -0,0 +1,32 @@
+﻿using FastReport;
+namespace FastReportHelper
+{
+    public class DataSetBuilder
+    {
+        public static void Build<T>(T o,string outputFileName, string DatasetName="Document",int maxNestingLevel=10)
+        {           
+            List<T> l = new List<T>
+            {
+                o
+            };
+            var r = new Report();
+            r.RegisterData(l, DatasetName, maxNestingLevel);
+            r.GetDataSource(DatasetName).Enabled = true;
+            r.Prepare();
+            r.Save(outputFileName); 
+        }
+
+        public static void Build<T>(string outputFileName, string DatasetName = "Document", int maxNestingLevel = 10) where T : new()
+        {           
+            List<T> l = new List<T>
+            {
+                new T()
+            };
+            var r = new Report();
+            r.RegisterData(l, DatasetName, maxNestingLevel);
+            r.GetDataSource(DatasetName).Enabled = true;
+            r.Prepare();
+            r.Save(outputFileName);
+        }
+    }
+}
---
+++ ReportBuilder.cs
@@ -0,0 +1,29 @@
+﻿using FastReport;
+using FastReport.Export.PdfSimple;
+
+
+namespace FastReportHelper
+{
+    public class ReportBuilder
+    {
+        public static void BuildPDF<T>(T o, string templateFileName,Stream s, string DatasetName="Document")
+        {
+            var l = new List<T>
+            {
+                o
+            };
+
+            var r = new Report();
+            r.Load(templateFileName);
+            r.RegisterData(l, DatasetName, 10);
+            r.Prepare();
+
+            PDFSimpleExport p = new PDFSimpleExport();
+            p.Export(r,s);
+        }
+
+
+
+
+    }
+}
